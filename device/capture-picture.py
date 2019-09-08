f3c73a77f52d22498ea78610d7c00d16dcab4303
Emily Xie <emilyxie@live.com>
2019-09-07 22:45:33 -0700

    adding device code

---
+++ device/capture-picture.py
@@ -0,0 +1,15 @@
+# record one picture per second, for 100 pictures
+from time import sleep
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (640,480)
+    camera.start_preview()
+    sleep(1)
+
+    for i, filename in enumerate(camera.capture_continuous('/home/pi/everysecond/upload/{timestamp:%Y%m%d-%H%M%S}.jpg')):
+        if i==10: 
+            break
+        sleep(1)
+    camera.stop_preview()
+
---
+++ device/upload-img.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+UPLOAD_FOLDER="/home/pi/everysecond/upload"
+
+while :
+do
+    WLAN=`ifconfig | grep wlan0`
+    # echo "Network is $WLAN"
+    if [ "$WLAN" != "" ]
+    then
+        OLDFILE=`ls -t $UPLOAD_FOLDER | tail -1`
+
+        OLDEST=$UPLOAD_FOLDER/$OLDFILE
+
+        if [[ "$OLDEST" == *".jpg"* ]]
+        then
+            echo "uploading to cloud"
+            gsutil cp $OLDEST gs://my-first-bucket-yay123456
+            if [ $(echo $?) == 0 ]
+            then
+                echo "removing file $OLDEST"
+                rm $OLDEST
+            fi
+        fi
+    fi
+done
